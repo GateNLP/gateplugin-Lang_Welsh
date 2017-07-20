8af1cc9687c5bb2e5cca72d09409cdff8af23bdb
Mark A. Greenwood <m.a.greenwood@sheffield.ac.uk>
2017-07-20 10:57:22 +0100

    first dump of code from svn

---
+++ src/main/java/wnlt/WelshTokeniser.java
@@ -0,0 +1,36 @@
+/*
+ *  WelshTokeniser.java
+ *  This file is part of Welsh Natural Language Toolkit (WNLT)
+ *  (see http://gate.ac.uk/), and is free software, licenced under 
+ *  the GNU Library General Public License, Version 2, June 1991
+ *  
+ */
+package wnlt;
+
+import gate.creole.ResourceReference;
+import gate.creole.metadata.CreoleParameter;
+import gate.creole.metadata.CreoleResource;
+import gate.creole.tokeniser.DefaultTokeniser;
+
+/**
+ * This class extends the DefaultTokeniser class of ANNIE.
+ * @author Andreas Vlachidis 20/03/2016
+ * 
+ */
+@CreoleResource(name="Welsh Tokeniser", icon="welsh_tokenizer.png")
+public class WelshTokeniser extends DefaultTokeniser {
+
+	private static final long serialVersionUID = 1L;
+
+  @Override
+  @CreoleParameter(defaultValue = "resources/Tokeniser/WelshTokeniser.rules")
+  public void setTokeniserRulesURL(ResourceReference tokeniserRulesURL) {
+    super.setTokeniserRulesURL(tokeniserRulesURL);
+  }
+
+  @Override
+  @CreoleParameter(defaultValue = "resources/Tokeniser/postprocess.jape")
+  public void setTransducerGrammarURL(ResourceReference transducerGrammarURL) {
+    super.setTransducerGrammarURL(transducerGrammarURL);
+  }
+}
